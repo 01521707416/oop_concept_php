092a566fbe67f4cd7c42734ed5d5be37bb950fc8
01521707416 <khalequzzaman16-464@diu.edu.bd>
2023-01-06 16:12:25 +0600

    first commit

---
+++ one.php
@@ -0,0 +1,23 @@
+<?php
+class Car
+{
+
+    function forward($car_name = "default")                         /* This is method */
+    {
+        return "$car_name is moving forward. <br>";
+    }
+
+    function backward($car_name = 'default')
+    {
+        return "$car_name is Moving reverse. <br>";
+    }
+}
+
+$obj = new Car;                                          /* Creating an object of the Car class */
+
+echo $obj->forward('Range Rover');                            /* -> is called an Accessor used in PHP oop concept */
+echo $obj->backward();
+
+$toyota = new Car;
+
+echo $toyota->backward('Toyota');
